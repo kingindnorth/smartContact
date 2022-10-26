534f728b4a879bc022b7fb44a2ff92eddfcbd39d
prajjawal <prajjawal.tiwari@niveussolutions.com>
2022-10-26 16:29:16 +0530

    debuged with sonarlint

--- src/main/java/com/smart/SmartcontactmanagerApplication.java
+++ src/main/java/com/smart/SmartcontactmanagerApplication.java
@@ -1,10 +1,9 @@
 package com.smart;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @SpringBootApplication
 public class SmartcontactmanagerApplication implements CommandLineRunner  {
--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,43 +1,13 @@
 package com.smart.controller;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
 import javax.servlet.http.HttpSession;
-import com.razorpay.*;
-
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.util.Streamable;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-
 import com.smart.dao.ContactRepository;
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
@@ -109,7 +79,7 @@ public class UserController {
 
 		List<Contact> contacts = this.contactRepository.getContactByUserId(user.getId());
 		model.addAttribute("contacts", contacts);	
-		System.out.println(contacts.get(0).getPhone());	
+		// System.out.println(contacts.get(0).getPhone());	
 
 
 		return "normal/show-contacts";
