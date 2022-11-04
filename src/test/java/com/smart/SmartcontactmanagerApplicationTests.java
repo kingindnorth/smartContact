9add8515ff91578906e256a9ec0d7be6186c224f
prajjawal <prajjawal.tiwari@niveussolutions.com>
2022-11-04 15:08:12 +0530

    deployment and service

--- src/test/java/com/smart/SmartcontactmanagerApplicationTests.java
+++ src/test/java/com/smart/SmartcontactmanagerApplicationTests.java
@@ -1,13 +1,44 @@
-// package com.smart;
+package com.smart;
 
-// import org.junit.jupiter.api.Test;
-// import org.springframework.boot.test.context.SpringBootTest;
+import org.apache.catalina.startup.UserConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
-// class SmartcontactmanagerApplicationTests {
+import com.smart.controller.HomeController;
+import com.smart.controller.UserController;
+import com.smart.dao.ContactRepository;
+import com.smart.entities.Contact;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-// 	@Test
-// 	void contextLoads() {
-// 	}
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
-// }
+
+@SpringBootTest
+class SmartcontactmanagerApplicationTests {
+
+	@Autowired
+	private UserController userController;
+
+	@MockBean
+	private ContactRepository repository;
+
+	@Test
+	public void getContactsTest() {
+		when(repository.findAll()).thenReturn(Stream
+				.of(new Contact(1,"test1","test11","test","test@gmail.com","123","image","desc"), new Contact(1,"test1","test11","test","test@gmail.com","123","image","desc")).collect(Collectors.toList()));
+		assertEquals(1, userController.showContacts());
+	}
+	@Test
+	public void deleteContactTest() {
+		Contact contact = new Contact(1,"test1","test11","test","test@gmail.com","123","image","desc");
+		userController.deleteContact(contact);
+		verify(repository, times(1)).delete(contact);
+	}
+
+}
